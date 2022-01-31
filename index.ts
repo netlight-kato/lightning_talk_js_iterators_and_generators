5fd7933aea252c597e50656a2ecc8aa1b242a7a9
Tobias Kahse <tobias.kahse@netlight.com>
2022-01-31 14:13:56 +0100

    Init

---
+++ index.ts
@@ -0,0 +1,3 @@
+// Lightning Talk: JavaScript Iterators and Generators
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
+// Reason: https://github.com/emma-sleep/payment-integration-service/pull/389/files#diff-1513a08d6f4c0586557665498a6f36d26b33836e6808a4d74953843556a931f9R120-R124
